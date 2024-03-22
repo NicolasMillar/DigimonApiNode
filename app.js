b1765dae23fb73ab752bba360417b0200c3fae0b
Nicolas <nicolas.millar.millar@gmail.com>
2024-03-22 17:04:16 -0300

    conneccion realizada a la bd de forma correcta

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
+var cors = require('cors')
+const port = process.env.PORT;
+
+
+//coneccion a la base de datos;
+const pgp = require('pg-promise')();
+const connectionString = process.env.DB_URL; 
+const db = pgp(connectionString);
+app.use(cors())
 
 app.use(express.json());
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
 
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo!');
 });
 
-app.listen(PORT, () => {
-  console.log(`La API está corriendo en http://localhost:${PORT}`);
-});
